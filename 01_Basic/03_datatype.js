884cbc5632bb86ca794f5595803cf0429a17c2e0
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-11 18:00:20 +0000

    datatypes

---
+++ 01_Basic/03_datatype.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3);    it'll give error
+
+const studentID = 1482;
+let emailID = "abc@gmail.com";
+var grade = 7.8;
+let address;
+let isStudent = true;
+
+console.log(typeof studentID);
+console.log(typeof emailID);
+console.log(typeof grade);
+console.log(typeof address);
+console.log(typeof isStudent);
