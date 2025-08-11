29eb739917c987953fdfbb861d5d302954df79e6
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-11 17:59:26 +0000

    Variables

---
+++ 01_Basic/02_variables.js
@@ -0,0 +1,12 @@
+const studentID = 1482;
+let emailID = "abc@gmail.com";
+var grade = 7.8;
+let address;
+
+// studentID = 2023 can't declare after assigning
+emailID = "xyz@gmail.com";
+grade = 8.13
+
+console.log(studentID);
+
+console.table([studentID, emailID, grade, address]);
