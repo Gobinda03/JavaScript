0ffcd7449e0e58dda8b46381bfb2ccaf8c7cb1e4
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-13 06:12:32 +0000

    updated

--- 01_Basic/04_conversionOperators.js
+++ 01_Basic/04_conversionOperators.js
@@ -18,3 +18,27 @@ let isloggedIn;
 let booleanIsLoggedIn = Boolean(isloggedIn);
 console.log(booleanIsLoggedIn); //by default false
 
+
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+let str1 = "hello";
+let str2 = " world";
+let str = str1 + str2;
+console.log(str);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(+true);
+
+
+
