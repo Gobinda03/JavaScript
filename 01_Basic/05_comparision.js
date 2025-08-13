5212e9b42ffbe4a77e7cfb73c68a2ee7afc787e4
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-13 06:20:14 +0000

    Comparisions

---
+++ 01_Basic/05_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(5 < 3);
+// console.log(5 <= 5);
+// console.log(5 == 3);
+// console.log(5 != 3);
+
+
+console.log("2" == 2);
+console.log("2" === 2); 
+
+console.log(null != 0);
+
+console.log(undefined == 0);
+
+console.log(undefined == null);
+console.log(undefined === null);
+
+
+
+
+
+
