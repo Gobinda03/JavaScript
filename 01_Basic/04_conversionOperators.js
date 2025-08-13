77a1547f3cc30837fab1d481bb77d53bc34c5bb0
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-13 05:16:11 +0000

    Conversion of datatype

---
+++ 01_Basic/04_conversionOperators.js
@@ -0,0 +1,20 @@
+let score = "50ac";
+let valueInNum = Number(score);
+
+console.log(typeof score);
+console.log(typeof(valueInNum));
+
+console.log(valueInNum);
+
+/*
+"50" => 50
+"50ac" => NaN
+"true" => 1
+
+*/
+
+
+let isloggedIn;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn); //by default false
+
