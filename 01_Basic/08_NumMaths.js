4ecfd07cabe5da99c3117868ad30a1550535e0a6
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-15 14:21:45 +0000

    Numbers and Maths

---
+++ 01_Basic/08_NumMaths.js
@@ -0,0 +1,23 @@
+const score = 100;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const rs = 10000000
+console.log(rs.toLocaleString('en-IN'));
+
+
+
+
+
+console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(4.8));
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
