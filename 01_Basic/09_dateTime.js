197143c11bcb433ad7918bdb6482aa82c27d545e
Gobinda Hazra <153456232+Gobinda03@users.noreply.github.com>
2025-08-15 14:36:15 +0000

    Date and Time

---
+++ 01_Basic/09_dateTime.js
@@ -0,0 +1,16 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023, 1, 23);
+let myCreatedDate = new Date("01-14-2025");
+
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
+
+
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getDay());
